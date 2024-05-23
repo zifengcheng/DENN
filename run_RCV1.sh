668dc48d69fe30b27f2fbb5dc7ed46be60a388bd
zifengcheng <75014054+zifengcheng@users.noreply.github.com>
2024-05-23 19:08:40 +0800

    Update run_RCV1.sh

--- run_RCV1.sh
+++ run_RCV1.sh
@@ -1,8 +1,7 @@
 alpha=$1
 temp=$2
-beta=$3
-k=$4
+k=$3
 for ((seed=2; seed<3; ++seed)); do
-    python run_baselines.py --random_seed $seed --dataset_name RCV1-V2 --root ../DataSet/RCV1-V2 --setting F --alpha ${alpha} --temp ${temp} --beta ${beta} --k ${k} --val_iter 2000
-    python test.py --random_seed $seed --dataset_name RCV1-V2 --root ../DataSet/RCV1-V2 --setting F --alpha ${alpha} --temp ${temp} --beta ${beta} --k ${k} --gpu 0,1,2,3 --batch_size 128
+    python run_baselines.py --random_seed $seed --dataset_name RCV1-V2 --root ../DataSet/RCV1-V2 --setting F --alpha ${alpha} --temp ${temp} --k ${k} --val_iter 2000
+    python test.py --random_seed $seed --dataset_name RCV1-V2 --root ../DataSet/RCV1-V2 --setting F --alpha ${alpha} --temp ${temp} --k ${k} --gpu 0,1,2,3 --batch_size 128
 done
