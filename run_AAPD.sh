15bf85382a56010a2a6063598ea54a04967d4a74
zifengcheng <75014054+zifengcheng@users.noreply.github.com>
2024-05-23 19:07:21 +0800

    Update run_AAPD.sh

--- run_AAPD.sh
+++ run_AAPD.sh
@@ -1,8 +1,7 @@
 alpha=$1
 temp=$2
-beta=$3
-k=$4
+k=$3
 for ((seed=2; seed<3; ++seed)); do
-    python run_baselines.py --random_seed $seed --dataset_name AAPD --root ../DataSet/AAPD --setting F --alpha ${alpha} --temp ${temp} --beta ${beta} --k ${k} --gpu 0,1 --val_iter 200
-    python test.py --random_seed $seed --dataset_name AAPD --root ../DataSet/AAPD --setting F --alpha ${alpha} --temp ${temp} --beta ${beta} --k ${k} --gpu 0,1,2,3 --batch_size 128
+    python run_baselines.py --random_seed $seed --dataset_name AAPD --root ../DataSet/AAPD --setting F --alpha ${alpha} --temp ${temp} --k ${k} --gpu 0,1 --val_iter 200
+    python test.py --random_seed $seed --dataset_name AAPD --root ../DataSet/AAPD --setting F --alpha ${alpha} --temp ${temp} --k ${k} --gpu 0,1,2,3 --batch_size 128
 done
