03d4bd29b894502805ead41760a1a67456b711f3
zifengcheng <75014054+zifengcheng@users.noreply.github.com>
2024-10-12 19:30:40 +0800

    Update run_Amazon.sh

--- run_Amazon.sh
+++ run_Amazon.sh
@@ -4,5 +4,5 @@ k=$3
 threshold=$4
 for ((seed=0; seed<1; ++seed)); do
     python run_baselines.py --random_seed $seed --dataset_name Amazon --root ../DataSet/Amazon --setting F --alpha ${alpha} --temp ${temp} --k ${k} --gpu 0,1 --val_iter 500
-    python test_item_model.py --random_seed $seed --dataset_name Amazon --root ../DataSet/Amazon --setting F --alpha ${alpha} --temp ${temp} --k ${k} --gpu 0,1,2,3 --batch_size 128 --thresh ${threshold}
+    python test.py --random_seed $seed --dataset_name Amazon --root ../DataSet/Amazon --setting F --alpha ${alpha} --temp ${temp} --k ${k} --gpu 0,1,2,3 --batch_size 128 --thresh ${threshold}
 done
